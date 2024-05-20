fb937afb3f955d03064ea196f13ca6a90ee15e32
djkostyan4ik <kostyamalinovskiy16@gmail.com>
2024-05-20 19:57:19 +0200

    2024-05-20 KM

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,14 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from bs4 import BeautifulSoup
+import pandas as pd
+import numpy as np
 import requests
-
+import json
+import os
+from app import utils
 
 @app.route('/')
-@app.route('/index')
 def index():
     return render_template('index.html')
 
@@ -18,23 +21,58 @@ def extract():
         response = requests.get(url)
         if response.status_code == requests.codes['ok']:
             page_dom = BeautifulSoup(response.text, 'html.parser')
-            try:
-                opinions_count = page_dom.select_one("a.product-review__link > span").get_text().strip()
-            except AttributeError:
-                opinions_count = 0
+            opinions_count = utils.extract(page_dom, 'a.product-review__link > span')
             if opinions_count:
-                # proces ekstrakcji
-                return redirect(url_for('product', product_id=product_id))
-            error = "Brak opinii."
+                url = f"https://www.ceneo.pl/{product_id}/opinie-1"
+                all_opinions = []
+                while (url):
+                    response = requests.get(url)
+                    page_dom = BeautifulSoup(response.text, 'html.parser')
+                    opinions = page_dom.select('div.js_product-review')
+                    for opinion in opinions:
+                        single_opinion = {
+                            key: utils.extract(opinion, *value)
+                                for key, value in utils.selectors.items()
+                        }
+                        all_opinions.append(single_opinion)
+                    try:
+                        url = 'https://ceneo.pl' + utils.extract(page_dom, 'a.pagination__next','href')
+                    except TypeError:
+                        url = None
+                if not os.path.exists('app/data'):
+                    os.mkdir('app/data')
+                if not os.path.exists('app/data/opinions'):
+                    os.mkdir('app/data/opinions')
+                with open(f'app/data/opinions/{product_id}.json','w', encoding='UTF-8') as jf:
+                    json.dump(all_opinions, jf, indent = 4, ensure_ascii=False)
+                opinions = pd.DataFrame.from_dict(all_opinions)
+                opinions.rating = opinions.rating.apply(lambda r: r.split('/')[0].replace(',','.'), ).astype(float)
+                opinions.recommendation = opinions.recommendation.apply(lambda r: 'Brak rekomendacji' if r is None else r)
+                stats = {
+                    "product_id": product_id,
+                    "opinions_count": opinions.shape[0],
+                    "pros_count": opinions.pros.apply(lambda p: 1 if p else 0).sum(),
+                    "cons_count": opinions.cons.apply(lambda c: 1 if c else 0).sum(),
+                    "average_rating": opinions.rating.mean(),
+                    "rating_distribution": opinions.rating.value_counts().reindex(np.arange(0,5.5,0.5), fill_value = 0),
+                    "recommendations_distribution": opinions.recommendation.value_counts().reindex(["Polecam", "Nie polecam", "Brak rekomendacji"])
+                }
+                if not os.path.exists('app/data/stats'):
+                    os.mkdir('app/data/stats')
+                with open(f'app/data/stats/{product_id}.json', 'w', encoding = 'UTF-8') as jf:
+                    return redirect(url_for('product', product_id=product_id))
+            error = "Dla produktu o podanym id nie ma opinii."
             return render_template('extract.html', error=error)
-        error = "Błędny kod."
+        error = "Produkt o podanym id nie istnieje."
         return render_template('extract.html', error=error)
     return render_template("extract.html")
 
 
 @app.route('/products')
 def products():
-    return render_template('products.html')
+    products = [filename.split('.')[0] for filename in os.listdir('app/data/opinions')]
+    
+    return render_template('products.html', products = products)
 
 @app.route('/author')
 def author():
@@ -42,5 +80,5 @@ def author():
 
 @app.route('/product/<product_id>')
 def product(product_id):
-    return render_template('product.html',product_id = product_id)
+    return render_template('product.html', product_id = product_id)
 
